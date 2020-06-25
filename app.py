ff3e7a884d04544314db7e8381515a5b0d968634
itachiuc <65366897+itachiuc@users.noreply.github.com>
2020-06-25 17:37:32 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask , render_template , flash , request , url_for 
+import numpy as np 
+import pandas as pandas
+import re 
+import os 
+import json
+
+app = Flask(__name__)
+
+global course,details
+
+f = open('courses.json','r')
+
+data = json.load(f)
+
+@app.route('/',methods=['GET','POST']) #flask decorator
+def home():
+	return render_template("tech.html")
+
+@app.route('/login',methods=['GET','POST']) #flask decorator
+def login():
+	return render_template("login.html")
+
+@app.route('/new',methods=['GET','POST']) #flask decorator
+def new():
+	return render_template("new.html")
+
+@app.route('/course/<course_name>',methods=['GET','POST'])
+def overview(course_name):
+	title = course_name
+	course = data['courses'][course_name]['name']
+	tagline = data['courses'][course_name]['tagline']
+	related = data['courses'][course_name]['related']
+	related_tag = data['courses'][course_name]['related_tag']
+	faq = data['courses'][course_name]['faq']
+	contents = data['courses'][course_name]['contents']
+	length = len(contents)
+	inner_contents = data['courses'][course_name]['inner_contents']
+	# print(details)
+	# print(contents)
+	# print(faq)
+
+	return render_template("courses.html",course=course,tagline=tagline,title=title,related=related,related_tag=related_tag,faq=faq,contents=contents,length=length,inner_contents=inner_contents)
+	
+
+if __name__ == '__main__':
+	app.run(debug=True)
